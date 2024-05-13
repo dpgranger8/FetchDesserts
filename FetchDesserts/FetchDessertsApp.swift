a341d2ee99adaef560772a5f778fcc4548f9b149
David Granger <dpgranger8@gmail.com>
2024-05-13 10:13:11 -0600

    Initial Commit

---
+++ FetchDesserts/FetchDessertsApp.swift
@@ -0,0 +1,17 @@
+//
+//  FetchDessertsApp.swift
+//  FetchDesserts
+//
+//  Created by David Granger on 5/13/24.
+//
+
+import SwiftUI
+
+@main
+struct FetchDessertsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
